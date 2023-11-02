79ce3284dcb4ecc3dea30e09e69e6f78352f26d8
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-11-01 23:47:33 -0300

    feat: configure react query client

--- App.tsx
+++ App.tsx
@@ -2,14 +2,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import Routes from "./src/routes";
 import AuthProvider from "./src/contexts/auth";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthProvider>
-        <StatusBar backgroundColor="#F0F4FF" style="dark" />
-        <Routes />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <StatusBar backgroundColor="#F0F4FF" style="dark" />
+          <Routes />
+        </AuthProvider>
+      </QueryClientProvider>
     </NavigationContainer>
   );
 }
