b1941f692ce142ca8b5f75a363f88b789ec5303a
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-11-01 23:48:46 -0300

    feature: create sign up function

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,6 +1,11 @@
 import { ReactNode, createContext, useState } from "react";
+import { useMutation } from "react-query";
+import ApiConfig from "../services/api";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AuthRoutesProps } from "../routes/auth.routes";
 
-type User = {
+export type User = {
   name: string;
   email: string;
   password: string;
@@ -8,18 +13,41 @@ type User = {
 
 type AuthContextProps = {
   user: User;
+  signUp: (userInput: User) => void;
 };
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
-export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
+export const AuthContext = createContext<AuthContextProps>(
+  {} as AuthContextProps
+);
 
 export default function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User);
 
+  const navigation =
+    useNavigation<NativeStackNavigationProp<AuthRoutesProps>>();
+
+  const createUser = useMutation({
+    mutationFn: async ({ name, email, password }: User) => {
+      await ApiConfig.post("/users", {
+        name,
+        email,
+        password,
+      });
+    },
+  });
+
+  function signUp({ name, email, password }: User) {
+    createUser.mutate({ name, email, password });
+    navigation.goBack();
+  }
+
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, signUp }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,21 +1,48 @@
 import { Platform } from "react-native";
 import { Background, Container, SubmitText } from "../SignIn/styles";
 import { Button, Input } from "../../components";
+import { useContext, useState } from "react";
+import { AuthContext, User } from "../../contexts/auth";
 
 export default function SignUp() {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { signUp } = useContext(AuthContext);
+
+  async function handleSignUp({ name, email, password }: User) {
+    signUp({ name, email, password });
+  }
+
   return (
     <Background>
       <Container
         behavior={Platform.OS === "ios" ? "padding" : undefined}
         enabled
       >
-        <Input placeholder="Seu nome" />
+        <Input
+          placeholder="Seu nome"
+          value={name}
+          onChangeText={(name) => setName(name)}
+        />
 
-        <Input placeholder="Seu email" keyboardType="email-address" />
+        <Input
+          placeholder="Seu email"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
 
-        <Input placeholder="Sua senha" />
+        <Input
+          placeholder="Sua senha"
+          value={password}
+          onChangeText={(password) => setPassword(password)}
+        />
 
-        <Button activeOpacity={0.8}>
+        <Button
+          activeOpacity={0.8}
+          onPress={() => handleSignUp({ name, email, password })}
+        >
           <SubmitText>Cadastrar</SubmitText>
         </Button>
       </Container>
