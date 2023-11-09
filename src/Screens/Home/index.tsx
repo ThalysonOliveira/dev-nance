f54b23776bbf49615ba11a3c34d0a4ec95787f69
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-11-08 22:47:22 -0300

    feat: create list balance cards

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,10 +1,47 @@
-import { Header } from "../../Components";
-import { Background } from "./styles";
+import { useQuery } from "react-query";
+import { BalanceItem, Header } from "../../Components";
+import { Background, ListBalance } from "./styles";
+import { ApiConfig } from "../../Services";
+import { useState } from "react";
+
+type Balance = {
+  saldo: number;
+  tag: string;
+};
 
 export default function Home() {
+  const [listBalance, setListBalance] = useState<Balance[]>([]);
+
+  useQuery({
+    queryKey: "getBalances",
+    queryFn: async () => {
+      const date = new Date();
+      const day = date.getDate();
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const formattedDate = `${day}/${month}/${year}`;
+
+      const { data } = await ApiConfig.get<Balance[]>("/balance", {
+        params: {
+          date: formattedDate,
+        },
+      });
+
+      setListBalance(data);
+    },
+  });
+
   return (
     <Background>
       <Header title="Minhas movimentações" />
+      <ListBalance
+        data={listBalance}
+        renderItem={({ item }: any) => (
+          <BalanceItem saldo={item.saldo} tag={item.tag} />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+      />
     </Background>
   );
 }
--- src/Screens/Home/styles.ts
+++ src/Screens/Home/styles.ts
@@ -5,3 +5,7 @@ export const Background = styled(SafeAreaView)`
     flex: 1;
     background-color:#F0F4FF ;
 `
+
+export const ListBalance = styled.FlatList`
+    max-height: 190px;
+`
